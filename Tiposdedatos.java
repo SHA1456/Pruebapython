1818814d814fb2a4fa5cd14940d44658bc76463e
SHA1456 <heredia.acero.sergio@iescamas.es>
2025-09-18 19:28:27 +0200

    Add files via upload

---
+++ Tiposdedatos.java
@@ -0,0 +1,34 @@
+package com;
+
+public class Tiposdedatos {
+
+	public static void main(String[] args) {
+		//Este es un tipo de dato númerico
+		/*
+		 * rgrgrr
+		 * grgr
+		 * rgrg
+		 * fdgdfgfdgd
+		 */
+		
+		
+		int numero=100;
+		float numero_decimal=1.1F;
+		double numero_decimal2=1.1;
+		boolean logico=false;
+		String palabra="Tomate";
+		char letra='d';
+		
+		System.out.println("Número"+100);
+		System.out.print("Numero decimal"+numero_decimal);
+		System.out.println("Lógico"+ logico);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
